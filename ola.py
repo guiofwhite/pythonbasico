61464f40561a36d00ecdc0d25299d8e30fac7acb
Gui <marinho.silva.guilherme@escola.pr.gov.br>
2026-02-09 14:11:09 +0000

    aula1

---
+++ ola.py
@@ -0,0 +1,7 @@
+#nome = input("Digite seu nome: ")
+#print("Olá,",nome, "seja bem vindo")
+
+#nome = input("Digite seu nome: ")
+#sobrenome = input("Digite seu Sobrenome: ")
+
+#print("Olá,", nome, sobrenome, "seja bem vindo")
