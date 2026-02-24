713a996ff21bc31f4eafa3fdc6f6d80325a3c792
Gui <marinho.silva.guilherme@escola.pr.gov.br>
2026-02-24 12:09:27 +0000

    versão02

--- Adivinhacao.py
+++ Adivinhacao.py
@@ -1,16 +1,37 @@
+import random
+
 print('**************************')
 print('*****Jogo adivinhação*****')
 print('**************************')
 
-numero_secreto = 39
+numero_secreto = random.randint(1,100)
+total_tentativas = 5
+rodada = 1
 
-chute_str = input("Digite o seu numero: ")
+while(rodada <= total_tentativas):
 
-print("Seu numero é: ", chute_str)
 
-chute = int(chute_str)
 
-if(numero_secreto == chute):
-    print("Você Acertou!! ")
-else:
-    print("Você errou KAKAKAKAKAKAKKAKA ")
+    chute_str = input("Digite o seu numero: ")
+
+    print("Seu numero é: ", chute_str)
+
+    chute = int(chute_str)
+
+    acertou = chute == numero_secreto
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
+
+
+    if(acertou):
+        print("Você Acertou!! ")
+        break
+    else:
+        if(maior):
+            print("O seu chute foi maior que o número secreto")
+        elif(menor):
+            print("O seu chute foi menor que o número secreto")
+    rodada = rodada + 1
+
+print(numero_secreto)
+print("Fim de jogo otáro")
