0e7151e3a7a9c7b93dc9e58a69a75ccaae6b6a82
Gui <marinho.silva.guilherme@escola.pr.gov.br>
2026-03-06 11:12:35 +0000

    listas

--- listas.py
+++ listas.py
@@ -1,6 +1,28 @@
-alunos = ["Ana", "Bruno", "Carlos", "Daniela"]
-alunos.append("João")
-alunos.remove("Ana")
+dados = ["Ana", 18, 1.65, True]
 
-for aluno in alunos:
-    print(aluno)
+turma = [
+    ["Ana", 18],
+    ["João", 17],
+    ["Maria", 16],
+]
+
+for turmas in turma:
+    print(turma)
+
+
+
+
+
+
+
+#print(dados)
+
+#for dado in dados:
+ #   print(dado)
+
+#alunos = ["Ana", "Bruno", "Carlos", "Daniela"]
+#alunos.append("João")
+#alunos.remove("Ana")
+#print(len(alunos))
+#for aluno in alunos:
+ #   print(aluno)
